0a300a90648c6f33e369a0f9337031150a483b86
chylli-deriv <chylli@deriv.com>
2024-01-18 10:14:11 +0800

    Revert "debug"
    This reverts commit 8f27cfff17c99a06247d32cbd47c29b523ad89b5.

--- src/parsers/perl-junit/java-stack-trace-element-parser.ts
+++ src/parsers/perl-junit/java-stack-trace-element-parser.ts
@@ -1,4 +1,3 @@
-import {info} from '@actions/core';
 export interface StackTraceElement {
   filePath: string
   lineStr: string
@@ -8,9 +7,7 @@ export interface StackTraceElement {
 const re = /\s+at (.*) line (\d+)\.$/
 
 export function parseStackTraceElement(stackTraceLine: string): StackTraceElement | undefined {
-
   const match = stackTraceLine.match(re)
-  info(`match ${stackTraceLine}: ${match}`)
   if (match !== null) {
     const [_, filePath, lineStr] = match
     return {

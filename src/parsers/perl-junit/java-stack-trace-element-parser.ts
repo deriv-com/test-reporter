8f27cfff17c99a06247d32cbd47c29b523ad89b5
chylli-deriv <chylli@deriv.com>
2024-01-18 09:21:09 +0800

    debug

--- src/parsers/perl-junit/java-stack-trace-element-parser.ts
+++ src/parsers/perl-junit/java-stack-trace-element-parser.ts
@@ -1,3 +1,4 @@
+import {info} from '@actions/core';
 export interface StackTraceElement {
   filePath: string
   lineStr: string
@@ -7,7 +8,9 @@ export interface StackTraceElement {
 const re = /\s+at (.*) line (\d+)\.$/
 
 export function parseStackTraceElement(stackTraceLine: string): StackTraceElement | undefined {
+
   const match = stackTraceLine.match(re)
+  info(`match ${stackTraceLine}: ${match}`)
   if (match !== null) {
     const [_, filePath, lineStr] = match
     return {
